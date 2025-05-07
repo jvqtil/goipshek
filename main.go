bc20696e571665fdf0d1489dd8e3cc8348638c3d
Egor <109822937+jvqtil@users.noreply.github.com>
2025-05-07 21:10:53 +0300

    fine it up!

--- main.go
+++ main.go
@@ -11,98 +11,97 @@ import (
 	"github.com/fatih/color"
 )
 
-type IpInfo struct {
+type ipInfo struct {
 	IP       string `json:"query"`
 	Country  string `json:"country"`
-	Region   string `json:"region"`
+	RG       string `json:"region"`
+	Region   string `json:"regionName"`
 	City     string `json:"city"`
 	ISP      string `json:"isp"`
 	Org      string `json:"org"`
 	AS       string `json:"as"`
 }
 
-func main() {
-	red := color.New(color.FgRed).SprintFunc()
-	blue := color.New(color.FgBlue).SprintFunc()
-	green := color.New(color.FgGreen).SprintFunc()
-	yellow := color.New(color.FgYellow).SprintFunc()
-	pink := color.New(color.FgMagenta).SprintFunc()
-	cyan := color.New(color.FgCyan).SprintFunc()
-	bold := color.New(color.Bold).SprintFunc()
+var (
+	red    = color.New(color.FgRed).SprintFunc()
+	blue   = color.New(color.FgBlue).SprintFunc()
+	green  = color.New(color.FgGreen).SprintFunc()
+	yellow = color.New(color.FgYellow).SprintFunc()
+	pink   = color.New(color.FgMagenta).SprintFunc()
+	cyan   = color.New(color.FgCyan).SprintFunc()
+	bold   = color.New(color.Bold).SprintFunc()
+)
 
+func main() {
 	var toCheck string
 	if len(os.Args) > 1 {
 		toCheck = os.Args[1]
 	} else {
-		resp, err := http.Get("https://api.ipify.org?format=json")
-		if err != nil {
-			log.Fatalf("Error getting IP: %v", err)
-		}
-		defer resp.Body.Close()
+		toCheck = getUserIP()
+	}
 
-		var ipResponse map[string]string
-		body, _ := ioutil.ReadAll(resp.Body)
-		err = json.Unmarshal(body, &ipResponse)
-		if err != nil {
-			log.Fatalf("Error parsing JSON: %v", err)
-		}
+	// Print all output
+	fmt.Println()
+	printer("IP", bold(toCheck), blue)
 
-		publicIP := ipResponse["ip"]
-		toCheck = publicIP
+	ipInfoNow := getInfo(toCheck)
+
+	if ipInfoNow.IP != toCheck {
+		printer("Raw IP", bold(ipInfoNow.IP), yellow)
 	}
 
 	fmt.Println()
-	fmt.Printf("%s %s\n",
-	blue(fmt.Sprintf("%-10s", "IP ")),
-	wrap(bold(toCheck)))
 
-	ipInfoResp, err := http.Get(fmt.Sprintf("http://ip-api.com/json/%s", toCheck))
+	printer("Country", ipInfoNow.Country, red)
+	RegionData := ipInfoNow.RG + " | " + ipInfoNow.Region
+	if ipInfoNow.RG != "" {
+		printer("Region", RegionData, green)
+	}
+	printer("City", ipInfoNow.City, blue)
+	printer("Provider", ipInfoNow.ISP, yellow)
+	printer("Org", ipInfoNow.Org, pink)
+	printer("AS", ipInfoNow.AS, cyan)
+}
+
+func wrap(arg string) string {
+	return "[ " + arg + " ]"
+}
+
+func printer(label string, value string, colorFunc func(a ...interface{}) string) {
+	if value != "" {
+		fmt.Printf("%s %s\n", colorFunc(fmt.Sprintf("%-10s", label)), wrap(value))
+	}
+}
+
+func getUserIP() string {
+	resp, err := http.Get("https://api.ipify.org?format=json")
+	if err != nil {
+		log.Fatalf("Error getting IP: %v", err)
+	}
+	defer resp.Body.Close()
+
+	var result struct{ IP string `json:"ip"` }
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		log.Fatalf("Error decoding IP: %v", err)
+	}
+	toCheck := result.IP
+
+	return toCheck
+}
+
+func getInfo(toCheck string) ipInfo {
+	resp, err := http.Get(fmt.Sprintf("http://ip-api.com/json/%s", toCheck))
 	if err != nil {
 		log.Fatalf("Error getting info by IP: %v", err)
 	}
-	defer ipInfoResp.Body.Close()
+	defer resp.Body.Close()
 
-	var ipInfoNow IpInfo
-	body, _ := ioutil.ReadAll(ipInfoResp.Body)
+	var ipInfoNow ipInfo
+	body, _ := ioutil.ReadAll(resp.Body)
 	err = json.Unmarshal(body, &ipInfoNow)
 	if err != nil {
 		log.Fatalf("Error parsing IP data: %v", err)
 	}
 
-	// Print all output
-	if ipInfoNow.IP != toCheck {
-		fmt.Printf("%s %s\n", 
-		yellow(fmt.Sprintf("%-10s", "Raw IP ")), 
-		wrap(bold(ipInfoNow.IP)))
-	}
-
-	fmt.Println()
-
-	fmt.Printf("%s %s\n", 
-	red(fmt.Sprintf("%-10s", "Country ")), 
-	wrap(ipInfoNow.Country))
-
-	fmt.Printf("%s %s\n", 
-	green(fmt.Sprintf("%-10s", "Region ")), 
-	wrap(ipInfoNow.Region))
-
-	fmt.Printf("%s %s\n", 
-	blue(fmt.Sprintf("%-10s", "City ")), 
-	wrap(ipInfoNow.City))
-
-	fmt.Printf("%s %s\n", 
-	yellow(fmt.Sprintf("%-10s", "Provider ")), 
-	wrap(ipInfoNow.ISP))
-
-	fmt.Printf("%s %s\n", 
-	pink(fmt.Sprintf("%-10s", "Org ")), 
-	wrap(ipInfoNow.Org))
-
-	fmt.Printf("%s %s\n", 
-	cyan(fmt.Sprintf("%-10s", "AS ")), 
-	wrap(ipInfoNow.AS))
-}
-
-func wrap(arg string) string {
-	return "[ " + arg + " ]"
+	return ipInfoNow
 }
