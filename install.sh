9ad35b3a95e87aef1c2b5a4bcccdd191ab0c9135
Egor <109822937+jvqtil@users.noreply.github.com>
2025-06-11 13:00:44 +0300

    added installer

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+OS="$(uname -s | tr '[:upper:]' '[:lower:]')"
+ARCH="$(uname -m)"
+FILE="unknown"
+
+case "$OS-$ARCH" in
+    linux-x86_64)   FILE="goipshek-linux-amd" ;;
+    linux-aarch64)  FILE="goipshek-linux-arm" ;;
+    darwin-arm64)   FILE="goipshek-macos-arm" ;;
+    *)
+        echo "Unsupported OS/arch: $OS-$ARCH"
+        exit 1
+        ;;
+esac
+
+URL="https://github.com/jvqtil/goipshek/releases/download/goipshek/$FILE"
+curl -L -O "$URL" || { echo "Download failed"; exit 1; }
+
+chmod +x "$FILE"
+echo "Successfully downloaded $FILE!"
+echo "Moving to /usr/local/bin, may require password"
+echo "If this fails please move the file to some bin directory in your PATH yourself"
+sudo mv $FILE /usr/local/bin/ipshek
+echo "Successfully moved to /usr/local/bin, now run with a simple *ipshek*"
